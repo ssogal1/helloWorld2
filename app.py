53c2d10af0379ec79009706459a97a8d4e495de9
shivani <ssogal1@terpmail.umd.edu>
2025-10-24 20:41:59 -0400

    Added base.html as parent code, contact.html for the contact form, and favorite-course.html to have users change site from URL.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -22,6 +22,24 @@ def about_css():
 @app.route('/greetings')
 def greetings():
     return render_template('greetings.html')
+
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        return render_template('contact.html', form_submitted=True, firstName=firstName, lastName=lastName, email=email, message=message)
+    return render_template('contact.html', form_submitted=False)
+
 
 if __name__ == '__main__':
     app.run()
